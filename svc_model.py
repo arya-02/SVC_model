1e7d9aa72df28e78bbbdf51ef4a4b6e9737c0cd0
arya <aryachak02@gmail.com>
2022-01-23 12:31:45 +0530

    first commit

---
+++ svc_model.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+
+#download data from kaggle
+data=pd.read_csv('Social_Network_Ads.csv')
+
+#slicing data for input cols and output col
+#two input col and one output col
+x=data.iloc[:,2:4].values
+y=data.iloc[:,4].values
+
+#import models
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=0,test_size=0.2)
+
+#scaling training data 
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.fit_transform(x_test)
+
+#training and predicting
+from sklearn.svm import SVC
+clf=SVC(gamma='auto')
+clf.fit(x_train,y_train)
+pred=clf.predict(x_test)
+
+#calculating accuracy
+from sklearn.metrics import accuracy_score
+score=accuracy_score(y_test,pred)
+print(score*100)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,pred)
+print(cm)
+
+#plotting graph
+
+#visualising training set
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+#visualising testing set
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+#saving the model in a file
+import pickle
+filename='model_svc.sav'
+pickle.dump(clf,open(filename,'wb'))
